d5e9834a30b614dbfa74e02adb0beeb925812cf9
Ostoic <dinnertable@hotmail.ca>
2018-01-04 16:23:21 -0500

    Added cmd line usage, and some comments.

--- linesofcode.cpp
+++ linesofcode.cpp
@@ -1,5 +1,4 @@
 // LoC.cpp : Defines the entry point for the console application.
-//
 
 #include <iostream>
 #include <fstream>
@@ -31,13 +30,10 @@ bool is_cpp_extension(const std::wstring& extension) noexcept
 	return std::find(extensions.begin(), extensions.end(), extension) != extensions.end();
 }
 
-bool contained_in_cwd(const fs::path& path)
+void display_usage() noexcept
 {
-	for (auto entry : fs::recursive_directory_iterator(fs::current_path()))
-		if (fs::equivalent(entry.path(), path))
-			return true;
-	
-	return false;
+	std::wcerr << "Usage: loc <folder path>\n";
+	std::wcerr << "LoC 1.0 by Shaun Ostoic\n";
 }
 
 int main(int argc, char* argv[])
@@ -45,28 +41,32 @@ int main(int argc, char* argv[])
 	if (argc < 2)
 	{
 		std::cerr << "Invalid parameters.\n";
-		std::cerr << "Usage: loc <folder path>\n";
+		display_usage();
 		return 0;
 	}
 
 	fs::path folderPath = argv[1];
 
+	// If the path is relative, convert it to an absolute path.
 	if (folderPath.is_relative())
 		folderPath = fs::absolute(folderPath);
 
+	// If the path does not exist, or is not a directory, 
 	if (!fs::is_directory(folderPath) || !fs::exists(folderPath))
 	{
-		std::cerr << "Given path is not a folder\n";
+		std::cerr << "Given path is not a folder.\n";
+		display_usage();
 		return 0;
 	}
 
 	std::size_t totalLines = 0;
 
+	std::wcout << "Counting lines of code...\n";
 	for (auto entry : fs::recursive_directory_iterator(folderPath))
 		if (!fs::is_directory(entry) && is_cpp_extension(entry.path().extension().wstring()))
 			totalLines += count_lines(entry) - 1;
 
-	std::wcout << "There are " << totalLines << " total lines of code in " << folderPath.wstring() << ".\n";
+	std::wcout << "There are " << totalLines << " total lines of code in " << folderPath.wstring() << "\n";
     return 0;
 }
 
