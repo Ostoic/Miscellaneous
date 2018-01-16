a7d7314802fd78c065f33f70bef2e01947f1c6cf
Ostoic <dinnertable@hotmail.ca>
2018-01-16 14:16:53 -0500

    Organized ct_map

--- ct_map.hpp
+++ ct_map.hpp
@@ -1,66 +1,40 @@
+#pragma once
+
 #include <array>
 #include <tuple>
 
 template <typename Key, typename Value, std::size_t Size = 5>
 class ct_map
 {
-public:
+public: // subtypes
 	using key_type = Key;
 	using value_type = Value;
 
 	using iterator = typename std::array<std::pair<Key, Value>, Size>::iterator;
 	using const_iterator = iterator;
 
-public:
+public: // interface
+	constexpr iterator begin() const noexcept;
+
+	constexpr iterator end() const noexcept;
+
+	constexpr std::size_t size() const noexcept;
+
+	constexpr std::size_t count(const key_type& key) const noexcept;
+
+	constexpr auto& find(const key_type& key) const;
+
+	constexpr const auto& operator[](const key_type& key) const;
+
+public: // {ctor}
 	constexpr ct_map() = default;
 
 	template <typename T, typename... Ts>
-	constexpr ct_map(T&& t, Ts&&... ts)
-		: data_(std::array<T, 1 + sizeof...(Ts)>{t, std::forward<Ts>(ts)...}) {}
+	constexpr ct_map(T&& t, Ts&&... ts);
 
-	constexpr iterator begin() const noexcept
-	{
-		return this->data_.begin();
-	}
-
-	constexpr iterator end() const noexcept
-	{
-		return this->data_.end();
-	}
-
-	constexpr std::size_t size() const noexcept
-	{
-		return this->data_.size();
-	}
-
-	constexpr std::size_t count(const key_type& key) const noexcept
-	{
-		std::size_t count = 0;
-        for (std::size_t i = 0; i < Size; i++)
-		{
-			if (std::get<0>(this->data_[i]) == key)
-				count++;
-		}
-
-		return count;
-	}
-
-	constexpr auto& find(const key_type& key) const
-	{
-        for (std::size_t i = 0; i < Size; i++)
-		{
-			if (std::get<0>(this->data_[i]) == key)
-				return std::get<1>(this->data_[i]);
-		}
-		
-		// Not found
-	}
-
-	constexpr const auto& operator[](const key_type& key) const
-	{
-		return this->find(key);
-	}
-
-private:
+private: // data
 	std::array<std::pair<key_type, value_type>, Size> data_;
 };
+
+// Implementation:
+#include "ct_map.hxx"
---
+++ impl/ct_map.hxx
@@ -0,0 +1,56 @@
+#pragma once
+#include "ct_map.hpp"
+
+template <typename K, typename V, std::size_t N = 5>
+template <typename T, typename... Ts>
+inline constexpr ct_map<K, V, N>::ct_map(T&& t, Ts&&... ts)
+	: data_(std::array<T, 1 + sizeof...(Ts)>{t, std::forward<Ts>(ts)...}) {}
+
+template <typename K, typename V, std::size_t N = 5>
+inline constexpr typename ct_map<K, V, N>::iterator 
+ct_map<K, V, N>::begin() const noexcept
+{
+	return this->data_.begin();
+}
+
+template <typename K, typename V, std::size_t N = 5>
+inline constexpr typename ct_map<K, V, N>::iterator 
+ct_map<K, V, N>::end() const noexcept
+{
+	return this->data_.end();
+}
+
+template <typename K, typename V, std::size_t N = 5>
+inline constexpr std::size_t ct_map<K, V, N>::size() const noexcept
+{
+	return this->data_.size();
+}
+
+template <typename K, typename V, std::size_t N = 5>
+inline constexpr std::size_t ct_map<K, V, N>::count(const key_type& key) const noexcept
+{
+	std::size_t count = 0;
+	for (std::size_t i = 0; i < N; i++)
+	{
+		if (std::get<0>(this->data_[i]) == key)
+			count++;
+	}
+
+	return count;
+}
+
+template <typename K, typename V, std::size_t N = 5>
+inline constexpr auto& ct_map<K, V, N>::find(const key_type& key) const
+{
+	for (std::size_t i = 0; i < N; i++)
+	{
+		if (std::get<0>(this->data_[i]) == key)
+			return std::get<1>(this->data_[i]);
+	}
+}
+
+template <typename K, typename V, std::size_t N = 5>
+inline constexpr const auto& ct_map<K, V, N>::operator[](const key_type& key) const
+{
+	return this->find(key);
+}
